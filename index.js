61d321e40c92c1755e5790bc51d75339ded7dba0
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-29 11:37:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names, type) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+    return messages
+}
+
+function countDown(int) {
+    
+    while (int >= 0) {
+      console.log(int--);
+    }
+
+}
